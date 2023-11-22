629f6147a010d19e01ac2e0e9223cf19ce4ab407
KimWash <ckm0728wash@gmail.com>
2023-11-22 14:18:20 +0900

    FEAT: App.js에 카운터 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useCallback } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{count}</p>
+        <button onClick={() => setCount(prev => --prev)}>-</button>
+        <button onClick={() => setCount(prev => ++prev)}>+</button>
         <a
           className="App-link"
           href="https://reactjs.org"
